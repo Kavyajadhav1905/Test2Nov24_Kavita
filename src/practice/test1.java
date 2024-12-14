ce9c85b8b0c32b55ae033bf22c6bee5f932ca1dc
Kavita Jadhav <kavyajadhav1905@gmail.com>
2024-12-14 10:42:39 +0530

    files added

--- src/practice/test1.java
+++ src/practice/test1.java
@@ -1,5 +1,8 @@
 package practice;
 
 public class test1 {
-
+	int i=0;
+	public static void main(String[] args) {
+		System.out.println("Hello_class");
+	}
 }
--- src/practice/test2.java
+++ src/practice/test2.java
@@ -1,5 +1,9 @@
 package practice;
 
 public class test2 {
-
+	int j=0;
+	public static void main(String[] args) {
+		System.out.println("Hello_class");
+	}
+	
 }
